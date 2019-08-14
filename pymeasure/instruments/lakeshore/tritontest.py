a64f6016b12d24ec87fe6d74ac59866687773342
kblagg04 <51968709+kblagg04@users.noreply.github.com>
2019-08-14 15:31:10 -0600

    Update tritontest.py

--- pymeasure/instruments/lakeshore/tritontest.py
+++ pymeasure/instruments/lakeshore/tritontest.py
@@ -8,10 +8,10 @@ import time
 import socket
 import numpy as np
 
-class TritonLakeshore(Instrument):
+class TritonLakeshore():
     """ Allows user to control Lakeshore370 via the Triton control software"""
 
-    def connect(self)
+    def connect(self):
         edsIP = "localhost"
         edsPORT = 33576
         MESSAGE = b'SET:DEV:UID:TEMP:MEAS:ENAB:\r\n'
@@ -20,22 +20,22 @@ class TritonLakeshore(Instrument):
         self.srvsock.settimeout(20) # 3 second timeout on commands
         self.srvsock.connect((edsIP, edsPORT))
 
-    def initalize_measure_T8(self) =
+    def initalize_measure_T8(self):
         self.srvsock.sendall(b'SET:DEV:T8:TEMP:LOOP:MODE:ON\r\n')
         self.srvsock.sendall(b'SET:DEV:T8:TEMP:LOOP:MODE:ON\r\n')
         self.srvsock.sendall(b'SET:DEV:T8:TEMP:MEAS:ENAB:ON\r\n')
                         
-    def initalize_measure_T8(self) =
+    def initalize_measure_T8(self):
         self.srvsock.sendall(b'SET:DEV:T8:TEMP:LOOP:MODE:ON\r\n')
         self.srvsock.sendall(b'SET:DEV:T8:TEMP:LOOP:MODE:ON\r\n')
         srvsock.sendall(b'SET:DEV:T8:TEMP:MEAS:ENAB:ON\r\n')
 
-    def get_temp_T8(self) = 
+    def get_temp_T8(self):
         self.srvsock.sendall(b'READ:DEV:T8:TEMP:SIG:TEMP\r\n')
         data = srvsock.recv(4096)
         return (data)
 
-    def get_temp_T5(self) = 
+    def get_temp_T5(self):
         self.srvsock.sendall(b'READ:DEV:T5:TEMP:SIG:TEMP\r\n')
         data = srvsock.recv(4096)
         return (data)
