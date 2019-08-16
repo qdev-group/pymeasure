6bb9758a35f09486a9fdb1d0ccd668d013aa8043
kblagg04 <51968709+kblagg04@users.noreply.github.com>
2019-08-16 17:05:46 -0600

    Update tritontest.py

--- pymeasure/instruments/lakeshore/tritontest.py
+++ pymeasure/instruments/lakeshore/tritontest.py
@@ -31,7 +31,7 @@ class TritonLakeshore():
         self.srvsock.sendall(b'SET:DEV:T8:TEMP:MEAS:ENAB:ON\r\n')
 
     def get_temp_T8(self):
-        self.srvsock.sendall(b'READ:DEV:T8:TEMP:SIG:TEMP\r\n')
+        self.srvsock.sendall(b'READ:DEV:T8:SIG:TEMP\r\n')
         data = self.srvsock.recv(4096)
         return (data)
 
