98ee168564b9760aa20742c43ab8947888787373
kblagg04 <51968709+kblagg04@users.noreply.github.com>
2019-08-26 16:28:22 -0600

    Update tritontest.py

--- pymeasure/instruments/lakeshore/tritontest.py
+++ pymeasure/instruments/lakeshore/tritontest.py
@@ -39,6 +39,44 @@ class TritonLakeshore():
         data = float(data[26:-2])
         return (data)
     
+    def get_tset_T8(self):
+        self.srvsock.sendall(b'READ:DEV:T8:TEMP:LOOP:TSET\r\n')
+        
+    def get_sweeprate(self):
+        self.srvsock.sendall(b'READ:DEV:T8:TEMP:RAMP:RATE\r\n')
+        
+    def set_temp_channel(self,channel):
+        self.srvsock.sendall(b'READ:DEV:T8:TEMP:LOOP:CHAN:{}\r\n'.format(channel))
+                             
+    def intialize_tempset_default(self,maxtemp):
+        #PID Settings
+        if maxtemp < 1.2:
+            self.srvsock.sendall(b'SET:DEV:T8:TEMP:LOOP:P:15\r\n')
+            self.srvsock.sendall(b'SET:DEV:T8:TEMP:LOOP:I:120\r\n')
+            self.srvsock.sendall(b'SET:DEV:T8:TEMP:LOOP:D:0\r\n')
+        else:
+            self.srvsock.sendall(b'SET:DEV:T8:TEMP:LOOP:P:3\r\n')
+            self.srvsock.sendall(b'SET:DEV:T8:TEMP:LOOP:I:10\r\n')
+            self.srvsock.sendall(b'SET:DEV:T8:TEMP:LOOP:D:0\r\n')
+        #Heater range
+        if maxtemp < 1:
+            self.srvsock.sendall(b'SET:DEV:T8:TEMP:LOOP:RANGE:10\r\n')
+        else:
+            self.srvsock.sendall(b'SET:DEV:T8:TEMP:LOOP:RANGE:3.16\r\n')
+        if maxtemp > 2:
+            print('Switch off the turbo, switch off the still heater, close V9, open V4, change channel to T5')
+
+    def set_PID_on(self):
+        self.srvsock.sendall(b'SET:DEV:T8:TEMP:LOOP:MODE:ON\r\n')
+    
+    def set_PID_off(self):
+        self.srvsock.sendall(b'SET:DEV:T8:TEMP:LOOP:MODE:OFF\r\n')
+    
+    def set_heater_range(self,htrrange):
+        self.srvsock.sendall(b'SET:DEV:T8:TEMP:LOOP:MODE:ON\r\n')
+        self.srvsock.sendall(b'SET:DEV:T8:TEMP:LOOP:RANGE:{}\r\n'.format(htrrange))
+        self.srvsock.sendall(b'SET:DEV:T8:TEMP:LOOP:MODE:OFF\r\n')
+    
     def set_temp_T8(self,tset):
         self.srvsock.sendall(b'SET:DEV:T8:TEMP:LOOP:{}\r\n'.format(tset))
         
