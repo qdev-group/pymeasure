bf25bbd59a7cd5df9c825a10994cb881424e34d1
kblagg04 <51968709+kblagg04@users.noreply.github.com>
2019-12-17 08:03:20 -0700

    Update IVswp_Keithley2400Keithleynvm.py

--- pymeasure/experiment/Kirsten/IVswp_Keithley2400Keithleynvm.py
+++ pymeasure/experiment/Kirsten/IVswp_Keithley2400Keithleynvm.py
@@ -1,12 +1,15 @@
-his is a generic code to measure resistance using DC Keithley 2400 and nvm
+##This is a generic code to measure resistance using DC Keithley 2400 and nvm, it does not record any other values
+## It will sweep a current from the set min current to the set max current with the given steps
 
 """
 
 ### Import ###
 
+#Required packages
 import numpy as np
 import time
 
+#Drivers
 from pymeasure.instruments.keithley import Keithley2400
 from pymeasure.instruments.keithley import Keithley2000
 
@@ -23,8 +26,7 @@ currentstep = 1e-8
 currentrange =  int((currentmax-currentmin)/currentstep+1)
 
 source.apply_current()
-print(nvm.voltage)
-print(nvm.beep_state)
+print(nvm.voltage) #Test the nvm connection
 nvm.beep_state = 'disabled'
 
 source.source_current = .1e-6
@@ -35,13 +37,14 @@ current = []
 voltage = []
 setcurrent = currentmin
 
+## IV sweep ##
 for i in range(currentrange):
     source.source_current = setcurrent
     current = np.append(current, float(source.source_current))
     voltage = np.append(voltage, float(nvm.voltage))
     setcurrent = setcurrent + currentstep
 
-
+## save the data ##
 IVsweep = np.asarray([current,voltage])
 IVsweep = np.transpose(IVsweep)
 
